ea176bc2b340e6728b8c4d05fd44d01046c37e6f
rogeriloboBR <matusael5004@hotmail.com>
2018-11-08 17:27:51 -0200

    Cap2C Eixo 1 ok
    Signed-off-by: rogeriloboBR <matusael5004@hotmail.com>

--- src/views/vida-segura/Cap31.vue
+++ src/views/vida-segura/Cap31.vue
@@ -1,6 +1,15 @@
 <template>
 	<div class="Cap31">
-		
+		<h3>1.	Introdução</h3>
+
+	<p>Este plano de ação divide-se em quatro partes. Inicialmente, é traçada a visão de futuro do plano, bem como os objetivos de longo prazo que se pretende com a sua implantação. Posteriormente, constam as metas globais para 2028, relacionadas aos resultados e impactos principais pretendidos, quais sejam, a redução do número de mortes e a diminuição dos principais fatores de risco à segurança viária. Tais metas dependem de um amplo conjunto de ações de todos os eixos de atuação.</p>
+	<p>Para cada eixo, são traçados objetivos, estratégias e diretrizes de atuação e ações específicas.</p>
+	<p>Explicita-se que foram adotadas simultaneamente as seguintes estratégias:</p>
+	<p>●	Prever diretrizes e estratégias de atuação para 2028;<br/>
+	   ●	Prever entregas específicas para 2019-2020<br/>
+	   ●	Prever a obrigação de publicação de novas entregas para os períodos 2021-2024 e 2025-2028 (a obrigação estará no texto do decreto de instituição deste plano).<br>
+	</p>
+
 	</div>
 </template>
 

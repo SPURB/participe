1dfebe022555c63b0b470064b941ec4c686f428e
davimh <hosogiri.davi@gmail.com>
2019-05-14 17:35:13 -0300

    ai2html processo piu, galerias, mobiliario-centro-aberto

--- src/components/GraficoLinha.vue
+++ src/components/GraficoLinha.vue
@@ -1,10 +1,13 @@
 <template>
 	<div class="GraficoLinha">
 		<svg ref="grafArea">
-			<line v-for="y in arr" x1="0" x2="700" :y1="y" :y2="y" stroke="#333" stroke-width="0.2"/>
-			<circle v-for="y in arr" :cy="y" :cx="(arr.indexOf(y) / (arr.length - 1)) * 700" r="10" fill="#333"/>
-			<text v-for="y in arr" :x="((arr.indexOf(y) / (arr.length - 1)) * 700) - 10" :y="y - 16">{{y}}</text>
-			<polyline :points="polylineRender(arr)" />
+			<!-- <line v-for="y in arr" x1="0" x2="700" :y1="y" :y2="y" stroke="#333" stroke-width="0.2"/> -->
+			<line v-for="step in max(arr, 10)" x1="0" x2="700" :y1="step" :y2="step" stroke="#333" stroke-width="0.2"/>
+			<circle v-for="point in arr" :cy="point.y" :cx="(arr.indexOf(point) / (arr.length - 1)) * 700" r="2" fill="#333"/>
+			<text v-for="point in arr" :x="((arr.indexOf(point) / (arr.length - 1)) * 700) - 10" :y="point.y - 12">{{ point.y }}</text>
+			<text v-for="point in arr" :x="((arr.indexOf(point) / (arr.length - 1)) * 700) - 10" :y="point.y - 28" style="font-size: x-small;">{{ point.x }}</text>
+			<text v-for="y in max(arr, 10)" x="0" :y="y + 16">{{ y }}</text>
+			<polyline :points="polylineRender(arr)" fill="none" stroke="#333" />
 		</svg>
 	</div>
 </template>
@@ -13,19 +16,35 @@ export default {
 	name: 'GraficoLinha',
 	data () {
 		return {
-			arr: [200, 150, 50, 100, 0]
+			arr: [ { "y": 480, "x": 2001}, { "y": 165, "x": 2002}, { "y": 147, "x": 2003}, { "y": 168, "x": 2004}, { "y": 22, "x": 2005}, { "y": 135, "x": 2006}, { "y": 33, "x": 2007}, { "y": 34, "x": 2008}, { "y": 0, "x": 2009}]
 		}
+
 	},
 	computed: {},
 	methods: {
-		polylineRender(arr) {
+		polylineRender(array) {
 			let points = ''
-			for (let i = 0; arr.indexOf(arr[i]) < arr.length; i++) {
-				// points =+ ((arr.indexOf(arr[i]) / (arr.length - 1)) * 700).toString() + ' ' +
-				// arr[i].toString + ', '
-				// console.log(i)
+			let ys = []
+			array.map(point => {
+				ys.push(point.y)
+			})
+			for (let i = 0; i < ys.length; i++) {
+				let point =+ ((i / (ys.length - 1)) * 700) + ',' + ys[i] + ' '
+				points = points + point
 			}
-			console.log(points)
+			return points
+		},
+		max(array, nSteps) {
+			let ys = []
+			array.map(point => {
+				ys.push(point.y)
+			})
+			let step = (Math.max(...ys) - Math.min(...ys)) / nSteps
+			let lineYs = []
+			for (let i = 0; i <= Math.max(...ys); i = i + step) {
+				lineYs.push(i)
+			}
+			return lineYs
 		}
 	},
 	mounted () {
--- src/views/ParqueMinhocao.vue
+++ src/views/ParqueMinhocao.vue
@@ -2,14 +2,30 @@
 	<div class="ParqueMinhocao" ref="conteudoConsulta">
 		<PageTop background_image_src="" :esta_consulta="estaConsulta" :social="social_assets">
 			<template slot="titulo"><div>PIU Parque Minhocão</div></template>
-			<template slot="subtitulo"><div>1ª consulta pública – Projeto de Intervenção Urbana do Parque Minhocão</div></template>
+			<template slot="subtitulo"><div>1ª consulta pública – Elementos prévios ao desenvolvimento do projeto</div></template>
 		</PageTop>
 		<Indice :titulos="titulosLimpo"></Indice>
 		<Apoio></Apoio>
-		<GraficoLinha></GraficoLinha>
-		<!-- <div class="linkMapaInterativo">
-			<a :href="imgSrc('mapas/piu-setor-central-2/')" target="_blank">MAPA INTERATIVO <i class="icon-acessar_url icon"><span>acessar_url</span></i></a>
-		</div> -->
+		<Processo></Processo>
+		<Galeria :gallery_attrs="galeria01"></Galeria>
+		<Galeria :gallery_attrs="galeria02"></Galeria>
+		<Galeria :gallery_attrs="galeria03"></Galeria>
+		<Galeria :gallery_attrs="galeria04"></Galeria>
+		<Galeria :gallery_attrs="galeria05"></Galeria>
+		<div class="mobiliario-centro-aberto">
+			<ul>
+				<li v-for="item in mobiliario_centro_aberto">
+					<img :src="item.ilustracao" :alt="item.titulo" :title="item.titulo">
+					<div class="nome">{{ item.titulo }}</div>
+					<ul>
+						<li v-for="caracteristica in item.caracteristicas">{{ caracteristica }}</li>
+					</ul>
+				</li>
+			</ul>
+			<div class="dados">
+				Exemplo de mobiliário no padrão Centro Aberto. Fonte: <a href="https://gestaourbana.prefeitura.sp.gov.br/wp-content/uploads/2015/07/Centro_Aberto_Pub.pdf">Centro Aberto: experiências na escala humana</a>. SMDU, PMSP.
+			</div>
+		</div>
 		<!-- <section ref="allComments">
 			<h2 v-show="commentsLoaded" class="titulo" indent="1">Contribuições</h2>
 			<CommentsLoader :attr="estaConsulta"></CommentsLoader>
@@ -30,10 +46,11 @@ import Tabela from '@/components/Tabela'
 import Imagem from '@/components/Imagem'
 import AntesDepois from '@/components/AntesDepois'
 import GraficoLinha from '@/components/GraficoLinha'
+import Processo from '@/components/graf/parque-minhocao/processo'
 import { consultasCommons } from '@/mixins/consultasCommons'
 
 export default {
-	name: 'SetorCentral2',
+	name: 'ParqueMinhocao',
 	data () {
 		return {
 			titulosLimpo: [],
@@ -41,9 +58,127 @@ export default {
 			consultas: false,
 			estaConsulta: {},
 			social_assets: {
-				whatsapp: '',
-				facebook: '',
-				twitter: ''
+				whatsapp: 'a',
+				facebook: 'a',
+				twitter: 'a'
+			},
+			mobiliario_centro_aberto: [{
+				'titulo': 'Bancos Modulares',
+				'ilustracao': this.$store.getters.basePath + 'arquivos/parque-minhocao/mobiliario-centro-aberto/1.jpg',
+				caracteristicas: [ 'Criam espaços para conversar', 'Conformam ‘espaços dentro do espaço’, convidando os passantes a sentar', 'Constroem grandes elementos para permanência', 'Tornam os espaços mais verdes com adição de vasos e planta' ]
+			}, {
+				'titulo': 'Banco e Mobiliário Portátil',
+				'ilustracao': this.$store.getters.basePath + 'arquivos/parque-minhocao/mobiliario-centro-aberto/2.jpg',
+				caracteristicas: [ 'Garantem múltiplas possibilidades de sentar ao ar livre', 'Conferem ao usuário o protagonismo no uso do espaço, movendo as cadeiras para a sombra, sentando em grupos etc.', 'Mesinhas na calçada dão apoio à comida de rua (barracas, food-trucks, food-bikes etc.)', 'Guarda-sóis proporcionam sombra nas áreas de permanência' ]
+			}, {
+				'titulo': 'Vasos',
+				'ilustracao': this.$store.getters.basePath + 'arquivos/parque-minhocao/mobiliario-centro-aberto/3.jpg',
+				caracteristicas: [ 'Ampliam a presença do verde na cidade', 'Fornecem sombra e filtrar a luz solar', 'Criam novos ambientes dentro do espaço', 'Servem como elementos adicionais de descanso', 'São uma alternativa aos balizadores para bloqueio do tráfego' ]
+			}, {
+				'titulo': 'Balizadores',
+				'ilustracao': this.$store.getters.basePath + 'arquivos/parque-minhocao/mobiliario-centro-aberto/4.jpg',
+				caracteristicas: [ 'Impedem a entrada de veículos sem interromper as linhas de desejo dos pedestres', 'Servem como bancos informais', 'Conformam pequenas bordas secundárias' ]
+			}, {
+				'titulo': 'Tratamento de piso',
+				'ilustracao': this.$store.getters.basePath + 'arquivos/parque-minhocao/mobiliario-centro-aberto/5.jpg',
+				caracteristicas: [ 'Unifica o espaço', 'Identifica com clareza as transformações do lugar', 'Cria identidade visual para intervenções pulverizadas', 'Conquista áreas de leito carroçável ocioso, mediando uma nova relação de uso do espaço viário' ]
+			}, {
+				'titulo': 'Iluminação e Projeção',
+				'ilustracao': this.$store.getters.basePath + 'arquivos/parque-minhocao/mobiliario-centro-aberto/6.jpg',
+				caracteristicas: [ 'Garantem espaços públicos mais seguros', 'Convidam ao uso noturno', 'Reanimam fachadas e muros sem janelas', 'Realçam detalhes das construções', 'Criam padrões luminosos de piso' ]
+			}, {
+				'titulo': 'Instalações Temporárias',
+				'ilustracao': this.$store.getters.basePath + 'arquivos/parque-minhocao/mobiliario-centro-aberto/7.jpg',
+				caracteristicas: [ 'Ativam bordas e fachadas degradadas ', 'Adicionam novas funções e atividades ao local', 'Criam unidades-satélite de atividades já presentes no entorno', 'Servem como unidades de depósito', 'Garantem a  presença  de  pessoas  no  local  ao  longo  do  dia,  fortalecendo a vigilância passiva' ]
+			}, {
+				'titulo': 'Centro de Informações e Apoio',
+				'ilustracao': this.$store.getters.basePath + 'arquivos/parque-minhocao/mobiliario-centro-aberto/8.jpg',
+				caracteristicas: [ 'Unidade de apoio e depósito de mobiliário portátil', 'Concentra informações sobre o projeto', 'Promove a zeladoria do espaço', 'Proporciona a interface com usuários' ]
+			}],
+			galeria01: {
+				width: 623,
+				height: 468,
+				images: [{
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q020/1.jpeg',
+					state: true,
+					legenda: 'Rua das Palmeiras, nº 58',
+					icon: false,
+					id: 1
+				}, {
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q020/2.jpeg',
+					state: false,
+					legenda: 'Rua Helvetia, nº 990',
+					icon: false,
+					id: 2
+				}]
+			},
+			galeria02: {
+				width: 623,
+				height: 468,
+				images: [{
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q041/1.jpeg',
+					state: true,
+					legenda: 'Avenida São João, nº 1475',
+					icon: false,
+					id: 1
+				}, {
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q041/2.jpeg',
+					state: false,
+					legenda: 'Rua Doutor Frederico Steidel, nº 120',
+					icon: false,
+					id: 2
+				}]
+			},
+			galeria03: {
+				width: 623,
+				height: 468,
+				images: [{
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q093/1.jpeg',
+					state: true,
+					legenda: 'Rua Dr. Frederico Steidel, nº 299',
+					icon: false,
+					id: 1
+				}, {
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q093/2.jpeg',
+					state: false,
+					legenda: 'Rua Dr. Frederico Steidel, nº 275',
+					icon: false,
+					id: 2
+				}]
+			},
+			galeria04: {
+				width: 623,
+				height: 468,
+				images: [{
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q094/1.jpeg',
+					state: true,
+					legenda: 'Rua Dr. Frederico Steidel, nº 233',
+					icon: false,
+					id: 1
+				}, {
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q094/2.jpeg',
+					state: false,
+					legenda: 'Rua Dr. Frederico Steidel, nº 211',
+					icon: false,
+					id: 2
+				}]
+			},
+			galeria05: {
+				width: 623,
+				height: 468,
+				images: [{
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q095/1.jpeg',
+					state: true,
+					legenda: 'Rua Sebastião Pereira, nº 98 até 76',
+					icon: false,
+					id: 1
+				}, {
+					url: this.$store.getters.basePath + 'arquivos/parque-minhocao/galerias/S007Q095/2.jpeg',
+					state: false,
+					legenda: 'Rua Sebastião Pereira, nº 60',
+					icon: false,
+					id: 2
+				}]
 			}
 		}
 	},
@@ -58,7 +193,8 @@ export default {
 		Tabela,
 		Imagem,
 		AntesDepois,
-		GraficoLinha
+		GraficoLinha,
+		Processo
 	},
 	methods: {
 		print () {
@@ -74,5 +210,72 @@ export default {
 @import '../consulta';
 @import '../oldstylestoprint';
 
-div.ParqueMinhocao {}
+div.ParqueMinhocao {
+	div.mobiliario-centro-aberto {
+		margin: 2rem auto;
+		padding: 2rem;
+		background-color: $cinza-3;
+		max-width: calc(100% - 400px - 4rem);
+		border-radius: 2px;
+		& > ul {
+			display: block;
+			display: flex;
+			flex-flow: row wrap;
+			justify-content: space-around;
+			align-items: flex-start;
+			min-width: 100%;
+			& > li {
+				display: inline-block;
+				vertical-align: top;
+				width: calc(320px - 2rem);
+				padding: 1rem;
+				text-align: center;
+				border: 1px solid $sombra-4;
+				border-radius: 1rem;
+				font-family: $grotesca;
+				background-color: #FFF;
+				img {
+					margin: 1rem;
+				}
+				& > div.nome {
+					font-size: small;
+					font-weight: bold;
+					color: #00b1ef;
+				}
+				& > ul {
+					text-align: left;
+					margin: 1rem 0 0 0;
+					& > li {
+						padding: 0;
+						margin: 0 0 0 1rem;
+						font-family: $grotesca;
+						font-size: small;
+					}
+				}
+			}
+		}
+		div.dados {
+			text-align: center;
+			font-size: small;
+			color: $cinza-2;
+		}
+		@media (max-width: 600px) {
+			width: 100%;
+			max-width: unset;
+			padding: 1rem;
+			& > ul {
+				padding: 0;
+				& > li:not(:last-child) {
+					margin: 0 0 1rem 0;
+					img {
+						max-width: 100%;
+					}
+				}
+				& > li:last-child {
+					margin: 0;
+				}
+			}
+		}
+	}
+}
 </style>

4ab52c66f8130542fccb6d505a005e713b534de2
davimh <hosogiri.davi@gmail.com>
2019-08-05 16:52:10 -0300

    Testando CommentsContext

--- src/components/CommentsContext.vue
+++ src/components/CommentsContext.vue
@@ -1,18 +1,20 @@
 <template>
-	<div class="comentavel" :class="{ aberto: abreComentario }">
-		<div @click="abreComentario = !abreComentario" :class="{ sucesso: sucesso }">
-			<div class="icon-counter">
-				<i class="icon-comentario icon"><span>chat</span></i>
-				<!-- filtrar numero (será refatorar CommentsLoader e vuex)-->
-				<!-- <span class="counter-comentario">89</span> -->
-			</div>
-			<div class="content-comentario">
+	<div class="comentavel" :class="{ aberto: abreComentario && consultaAtiva }">
+		<div class="cortina"></div>
+		<div :class="{ sucesso: sucesso }">
+			<div class="content-comentario" @click="toggleForm()">
+				<div class="icon-counter" @click="toggleForm()">
+					<i v-if="!abreComentario" class="icon-comentario icon" @click="toggleForm()"><span>comentario</span></i>
+					<!-- filtrar numero (será refatorar CommentsLoader e vuex)-->
+					<span v-if="!abreComentario" class="counter-comentario" @click="toggleForm()">89</span>
+					<i v-else class="icon-incorreto icon" @click="toggleForm()"><span>incorreto</span></i>
+				</div>
+				<h1>Escrevendo contribuição sobre o trecho:</h1>
 				<slot></slot>
 			</div>
 		</div>
-		<transition name="form_display">
-			<form v-if="toggleFormOrMessage">
-				<h3 class="form_title">Comente aqui</h3>
+		<form v-if="consultaAtiva">
+			<aside>
 				<fieldset>
 					<label for="nome">Nome</label>
 					<input
@@ -53,6 +55,8 @@
 						v-model='form_email'
 					>
 				</fieldset>
+			</aside>
+			<main>
 				<fieldset>
 					<label for="comentario">Comente aqui</label>
 					<textarea
@@ -73,9 +77,9 @@
 					</svg>
 					<a @click="checkName" :class="{ enviando: enviandoComment, erro: erro }"></a>
 				</div>
-			</form>
-			<p class="consulta-encerrada" v-if="!consultaAtiva">Desculpe, mas o período de participação já foi encerrado.</p>
-		</transition>
+			</main>
+		</form>
+		<p class="consulta-encerrada" v-if="!consultaAtiva">Desculpe, mas o período de participação já foi encerrado.</p>
 	</div>
 </template>
 
@@ -101,10 +105,32 @@ export default {
 			default: 1
 		}
 	},
+
 	mixins: [ commentsCommons ],
+
 	data () {
 		return {
+			abreComentario: false,
 			form_context: null
+		}
+	},
+
+	watch: {
+		abreComentario () {
+			document.body.style.overflow = document.body.style.overflow === '' ? 'hidden' : ''
+			if (this.abreComentario) {
+				window.scrollTo({
+					top: this.$el.offsetTop,
+					left: 0,
+					behavior: 'smooth'
+				})
+			} else {
+				window.scrollTo({
+					top: this.$el.offsetTop - 60,
+					left: 0,
+					behavior: 'smooth'
+				})
+			}
 		}
 	},
 
@@ -122,6 +148,9 @@ export default {
 	},
 
 	methods: {
+		toggleForm () {
+			this.abreComentario = !this.abreComentario
+		},
 		send () {
 			let app = this
 			app.erro = false
@@ -159,158 +188,144 @@ export default {
 @import '../variables';
 
 .comentavel {
-	margin:  0 auto;
-	max-width: 700px;
-	border-radius: 2px;
-	overflow: hidden;
-	transition: all ease-in-out .2s;
 	position: relative;
-	z-index: 0;
-
-	.consulta-encerrada{
-		display: none;
-		background-color:$vermelho;
-		padding: 1em;
-		color: #FFF
+	transition: padding ease-in-out .4s;
+	div.cortina {
+		position: absolute;
+		top: 0;
+		width: 100vw;
+		height: 0;
+		z-index: 1;
 	}
-
-	&:hover,
-	&.aberto {
-		background: $cinza-3;
-	}
-	&.aberto {
-		padding: 1em 0;
-		margin: 1em auto;
-		.consulta-encerrada{
-			display: block
-		}
-	}
-
-	& > div {
-		display: grid;
-		grid-gap: 10px;
-		grid-template-columns: 30px 1fr;
-
-		.icon-counter{
-			display: flex;
-			flex-direction: column;
-			.counter-comentario,
-			.icon-comentario {
-				color: $cinza-2;
-				text-align: center;
-				transition: color ease-in-out .2s;
-			}
-			.icon-comentario{
-				margin-top: .5rem;
-			}
-		}
+	& > div:not(.cortina) {
+		position: relative;
+		margin: 0 auto 1rem;
+		max-width: calc(700px - 4rem + 2rem);
+		background-color: $cinza-3;
+		border-radius: 4px;
+		cursor: pointer;
+		transition: all ease-in-out .2s;
+		z-index: 2;
 		&:hover {
-			cursor: pointer;
-			.counter-comentario,
-			.icon-comentario {
-				color: $preto
+			background-color: transparent;
+		}
+		.content-comentario {
+			padding: 0.5rem 0;
+			.icon-counter {
+				position: absolute;
+				top: -4px;
+				right: -4rem;
+				& > * {
+					color: $cinza-2;
+					cursor: pointer;
+					transition: all ease-in-out .2s;
+				}
+				i.icon {
+					font-size: 1.5rem;
+					&.icon-incorreto {
+						color: $vermelho;
+						&::before {
+							font-size: 2.5rem;
+						}
+					}
+				}
+				.counter-comentario {
+					display: block;
+					text-align: center;
+					line-height: 1;
+				}
 			}
-		};
-
-		&.sucesso {
-			margin-bottom: 1em;
-			&::after {
-				content: 'Comentário enviado';
-				line-height: 1.3;
-				background-color: $verde;
-				padding: .7em;
-				color: #fff;
-				grid-column: 1 / 3;
-				grid-row: 2
+			h1 {
+				margin: 0.5rem 0 0;
+				font-size: 1rem;
+				color: $cinza-1;
+				max-height: 0;
+				overflow: hidden;
+				transition: all ease-in-out .2s;
+			}
+			& > * {
+				padding-right: 1rem;
+				padding-left: 1rem;
+			}
+			& > *:last-child, & > *:last-child > *:last-child {
+				margin-bottom: 0;
+			}
+			&:hover {
+				.icon, .counter-comentario { color: $preto; }
+			}
+			&::before {
+				content: '“';
+				position: absolute;
+				top: 3.5rem;
+				left: -2rem;
+				font-family: $serifada;
+				font-size: 6rem;
+				color: $cinza-2;
+				opacity: 0;
+				transition: opacity ease-in .05s;
 			}
 		}
 	}
-
-	& div.content-comentario{
-		p, ol, ul {
-			padding-left: 0
-		}
-	}
-
-	form {
-		background: $cinza-3;
-		display: block;
-		padding: 0 2rem 2rem 2rem;
-		height: 100%;
-		&.form_display-enter,
-		&.form_display-leave-to{
-			height: 0;
-		}
-
-		.form_title{
-			border-top: solid 1px $cinza-1;
-			padding-top: 1em
-		}
-
+	& > form {
+		max-width: calc(700px - 4rem + 2rem);
+		display: grid;
+		grid-template-columns: 1fr 2fr;
+		grid-gap: 2rem;
+		padding: 0 1rem;
+		margin: 0 auto;
+		max-height: 0;
+		overflow: hidden;
+		transition: all ease-in-out .4s;
+		z-index: 2;
 		fieldset {
 			margin: 0;
 			padding: 0;
 			border: 0;
-
 			label {
 				display: block;
-				font-size: initial;
+				font-size: 0.8rem;
 				font-weight: initial;
-				height: 30px;
-				line-height: 30px;
+				text-transform: uppercase;
+				color: $cinza-1;
 			};
-
 			input, textarea {
-				display: block;
 				width: 100%;
 				font-family: inherit;
 				font-size: large;
-				height: 40px;
-				line-height: 40px;
-				border: 1px solid $cinza-2;
+				height: 2rem;
+				line-height: 1.6;
+				border: 1px solid $sombra-4;
 				border-radius: 2px;
 				padding: 8px;
 				caret-color: $vermelho;
-				box-shadow: inset 0 2px 2px rgba(0, 0, 0, .08);
-				margin-bottom: .8rem;
+				&:not(:last-child) {
+					margin-bottom: 1rem;
+				}
 				transition: all ease-in .1s;
-
 				&:focus { border-color: $vermelho; };
-
 				&.inputErro {
 					background: $vermelho;
 					color: #FFF;
 				};
-
 				&.inputAcerto { color: $verde; };
 			};
-
 			textarea {
-				height: 12rem;
-				line-height: 160%;
-				padding: 4px 8px;
+				padding: 0 8px;
+				height: 10rem;
+				margin-bottom: 1rem;
 			};
 		};
-
 		div.action {
-			margin-top: 1rem;
-			height: 42px;
-			position: relative;
-			text-align: right;
-
 			svg {
 				position: absolute;
 				right: 0;
 				top: 0;
 				animation: surge ease-in .48s;
-
 				@keyframes surge {
 					from { opacity: 0 }
 					to { opacity: 1 }
 				}
-
 				.bolinha1, .bolinha2, .bolinha3 { fill: #FFF; }
-
 				@keyframes pulando {
 					0% {
 						transform: translateY(4px);
@@ -332,12 +347,10 @@ export default {
 					animation: pulando 0.6s 0.45s infinite;
 					transition: cubic-bezier(0.25, 0.46, 0.45, 0.94);
 				}
-
 				.bolinha2 {
 					animation: pulando 0.6s 0.55s infinite;
 					transition: cubic-bezier(0.25, 0.46, 0.45, 0.94);
 				}
-
 				.bolinha3 {
 					animation: pulando 0.6s 0.66s infinite;
 					transition: cubic-bezier(0.25, 0.46, 0.45, 0.94);
@@ -345,7 +358,7 @@ export default {
 			}
 			a {
 				display: inline-block;
-				color: inherit;
+				color: #FFF;
 				font-family: inherit;
 				font-size: smaller;
 				text-transform: uppercase;
@@ -353,26 +366,24 @@ export default {
 				overflow: hidden;
 				width: 100%;
 				text-align: center;
-				line-height: 40px;
-				border-radius: 2px;
-				border: 1px solid $cinza-2;
-				background: #FFF;
-				box-shadow: 0 2px 2px rgba(0, 0, 0, .24);
-				transition: all ease-out .36s;
+				line-height: 3rem;
+				border-radius: 4px;
+				background: $verde;
+				transition: all ease-out .2s;
 				position: relative;
 				white-space: nowrap;
-				&::after { content: 'Comentar'; }
+				&::after { content: 'Enviar contribuição'; }
+				&:hover {
+					background-color: $verde-tr;
+				}
 				&.enviando {
 					max-width: 0;
-					border-color: transparent;
-					box-shadow: none;
 					background: transparent;
 				}
 				&.erro {
 					background: $vermelho;
 					color: #FFF;
 					border-color: transparent;
-
 					&::after { content: 'Tentar novamente'; }
 				}
 				&.erro.enviando { background: transparent; }
@@ -380,24 +391,135 @@ export default {
 				&:focus { border-color: $vermelho; };
 			};
 		};
-	};
-	@media (max-width: 600px) {
-		.content-comentario {
-			padding-left:0;
-			margin-left: 0
+	}
+	& > p.consulta-encerrada {}
+	&:active {
+		& > div:not(.cortina) {
+			background-color: $vermelho-tr;
+			transition: none;
 		}
 	}
-	@media print {
-		max-width: unset;
-		user-select: none;
-		& > div {
-			display: block;
-			.icon-counter { display: none; }
+	&.aberto {
+		background-color: #FFF;
+		padding: 4rem 0;
+		height: 100vh;
+		overflow-y: auto;
+		div.cortina {
+			height: 100vh;
+		}
+		& > div:not(.cortina) {
+			background-color: transparent;
 			.content-comentario {
-				padding: 0 20mm;
-				p, ol { padding-right: 0; }
+				h1 {
+					margin: 0 0 4rem;
+					max-height: 4rem;
+				}
+				&::before {
+					opacity: 1;
+					transition: opacity ease-in-out .4s;
+					transition-delay: .2s;
+				}
 			}
 		}
+		& > form {
+			margin: 4rem auto;
+			max-height: 100vh;
+		}
 	}
-};
+}
+
+// .comentavel {
+// 	margin: 0 auto;
+// 	max-width: 700px;
+// 	border-radius: 2px;
+// 	overflow: hidden;
+// 	transition: all ease-in-out .2s;
+// 	position: relative;
+// 	z-index: 0;
+
+// 	.consulta-encerrada{
+// 		display: none;
+// 		background-color: $vermelho;
+// 		padding: 1em;
+// 		color: #FFF
+// 	}
+
+// 	&:hover,
+// 	&.aberto {
+// 		background: $cinza-3;
+// 	}
+// 	&.aberto {
+// 		padding: 1em 0;
+// 		margin: 1em auto;
+// 		.consulta-encerrada{
+// 			display: block
+// 		}
+// 	}
+
+// 	& > div {
+// 		display: grid;
+// 		grid-gap: 10px;
+// 		grid-template-columns: 30px 1fr;
+
+// 		.icon-counter{
+// 			display: flex;
+// 			flex-direction: column;
+// 			.counter-comentario,
+// 			.icon-comentario {
+// 				color: $cinza-2;
+// 				text-align: center;
+// 				transition: color ease-in-out .2s;
+// 			}
+// 			.icon-comentario{
+// 				margin-top: .5rem;
+// 			}
+// 		}
+// 		&:hover {
+// 			cursor: pointer;
+// 			.counter-comentario,
+// 			.icon-comentario {
+// 				color: $preto
+// 			}
+// 		};
+
+// 		&.sucesso {
+// 			margin-bottom: 1em;
+// 			&::after {
+// 				content: 'Comentário enviado';
+// 				line-height: 1.3;
+// 				background-color: $verde;
+// 				padding: .7em;
+// 				color: #fff;
+// 				grid-column: 1 / 3;
+// 				grid-row: 2
+// 			}
+// 		}
+// 	}
+
+// 	& div.content-comentario{
+// 		p, ol, ul {
+// 			padding-left: 0
+// 		}
+// 	}
+
+	
+// 	@media (max-width: 600px) {
+// 		.content-comentario {
+// 			padding-left:0;
+// 			margin-left: 0
+// 		}
+// 	}
+// 	@media print {
+// 		max-width: unset;
+// 		user-select: none;
+// 		& > div {
+// 			display: block;
+// 			.icon-counter { display: none; }
+// 			.content-comentario {
+// 				padding: 0 20mm;
+// 				p, ol { padding-right: 0; }
+// 			}
+// 		}
+// 	}
+// };
 </style>

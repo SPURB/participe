9bf1779ba847d73d34c45b38849c845f43adce58
yubathom <yubathom@gmail.com>
2019-09-25 15:37:35 -0300

    Finaliza testes e2e de api e home

--- tests/e2e/integration/0_api.js
+++ tests/e2e/integration/0_api.js
@@ -1,31 +1,17 @@
-describe('API Headers', () => {
-	let VUE_APP_API_URL
-	let apiVersion
-
-	before(() => {
-		VUE_APP_API_URL = Cypress.env('VUE_APP_API_URL')
-		apiVersion = Cypress.env('apiVersion')
-	})
-
-	it('Loga variáveis de ambiente', () =>{
-		cy.log(VUE_APP_API_URL)
-		cy.log(apiVersion)
-	})
-
+describe('API', () => {
 	it('Retorna content-type JSON', () => {
-		cy.request(`${VUE_APP_API_URL}${apiVersion}/consultas`)
+		cy.request(`${Cypress.env('VUE_APP_API_URL')}${Cypress.env('apiVersion')}/consultas`)
 			.its('headers')
 			.its('content-type')
-			.should('include', 'application/json')
+			.should('include', 'application/json', 'Header consta "content-type: application/json"')
 	})
 
 	it('Retorna número correto de consultas a partir da tabela "consultas"', () => {
-		cy.request(`${VUE_APP_API_URL}${apiVersion}/consultas`)
+		cy.request(`${Cypress.env('VUE_APP_API_URL')}${Cypress.env('apiVersion')}/consultas`)
 			.its('body')
 			.then(body => {
-				expect(Object.prototype.toString.call( body ) === '[object Array]').to.equal(true, 'Requisição retorna um array')
-				expect(body.length > 10).to.equal(true, 'Requisição retorna mais que 10 itens')
+				expect(Object.prototype.toString.call(body) === '[object Array]').to.equal(true, 'Requisição retorna um array')
+				expect(body.length > 10).to.equal(true, 'Há mais que 10 itens no array')
 			})
 	})
-
 })

be9b33bc8fdd8b3e1e095adb357e03efd3adc430
Thomas Yuba <yubathom@gmail.com>
2019-09-25 16:09:54 -0300

    Tests/e2e setup (#117)
    * Setup de testes e2e
    
    * Finaliza testes e2e de api e home

--- tests/e2e/_eslintrc.js
+++ tests/e2e/_eslintrc.js
@@ -6,7 +6,15 @@ module.exports = {
     mocha: true,
     'cypress/globals': true
   },
-  rules: {
-    strict: 'off'
-  }
+	rules: {
+		'indent': [1, 'tab'],
+		'no-tabs': 0,
+		'no-irregular-whitespace': 0,
+		'vue/require-v-for-key': 0,
+		'no-array-constructor': 0,
+		'camelcase': 0
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 }

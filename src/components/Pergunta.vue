0080e35d553f84202513e6d9df488a9204a708f8
davimh <hosogiri.davi@gmail.com>
2019-10-17 17:00:10 -0300

    Finaliza localStorage

--- src/components/Pergunta.vue
+++ src/components/Pergunta.vue
@@ -9,12 +9,12 @@
 				<i class="icon-incorreto icon" @click="toggleModal($event)"><span>incorreto</span></i>
 			</div>
 		</main>
-		<aside>
+		<aside :class="{ tosend: step == 2, enviando: enviandoComment }">
 			<button class="formStart" :class="{ hidden: step !== 0 }" @click="goStep(1)">
 				<i class="icon-responder icon"><span>responder</span></i>
 				Escreva sua resposta
 			</button>
-			<form :class="{ hidden: step == 0 }">
+			<form :class="{ hidden: step == 0 || step > 2 }" >
 				<fieldset :class="{ hidden: step !== 1 }">
 					<div>
 						<label for="nome">Nome</label>
@@ -26,6 +26,7 @@
 							v-validate="'required: true'"
 							:class="{ inputErro: errors.has('name') }"
 							v-model='form_name'
+							ref="nome"
 						>
 					</div>
 					<div>
@@ -65,7 +66,6 @@
 					</div>
 				</fieldset>
 				<fieldset :class="{ hidden: step !== 2 }">
-					<label for="comentario">Comente aqui</label>
 					<textarea
 						value=""
 						id="comentario"
@@ -73,13 +73,29 @@
 						v-validate="'required: true'"
 						:class="{ inputErro: errors.has('content') }"
 						v-model='form_content'
+						placeholder="Escreva sua resposta aqui"
 					></textarea>
 				</fieldset>
 			</form>
-			<button class="formBtn" :class="{ hidden: step == 0, prox: step == 1, enviar: step == 2 }" @click="formButton">
+			<button class="formBtn" :class="{ hidden: step == 0 || step > 2 || errors.items.length > 0, prox: step == 1, enviar: step == 2 }" @click.prevent="formButton">
 				<span>Enviar</span>
 				<i class="icon-seta_direita icon"><span>seta_direita</span></i>
 			</button>
+			<div :class="{ hidden: step < 3, enviando: enviandoComment, erro: erro, sucesso: sucesso }">
+				<template v-if="enviandoComment">
+					<p class="enviando">Enviando...</p>
+				</template>
+				<template v-if="erro">
+					<p>Ocorreu um erro ao enviar sua resposta</p>
+					<button @click="send">
+						<i class="icon-novamente icon"><span>novamente</span></i>
+						<span>Tentar novamente</span>
+					</button>
+				</template>
+				<template v-if="sucesso">
+					<p>Sua resposta foi enviada</p>
+				</template>
+			</div>
 		</aside>
 	</div>
 </template>
@@ -103,9 +119,13 @@ export default {
 		}
 	},
 	mixins: [ commentsCommons ],
+	created () {
+		this.checkStorage()
+	},
 	methods: {
 		send () {
 			let app = this
+			this.step++
 			app.erro = false
 			app.enviandoComment = true
 			api.post(process.env.VUE_APP_API_URL + 'members', {
@@ -117,10 +137,9 @@ export default {
 				'trash': '0',
 				'postid': '1',
 				'commentid': app.attr.id,
-				'commentcontext': 'contexto!'
+				'commentcontext': 'contexto!' // ADICIONAR CONTEXTOOOO
 			})
 				.then(function (response) {
-					console.log(app.attr.id)
 					app.abreComentario = false
 					app.sucesso = true
 					app.resetForm()
@@ -137,14 +156,31 @@ export default {
 			this.step = num
 		},
 		toggleModal (event) {
-			// event.stopPropagation()
-			// this.$el.addEventListener('click', (event => {
-			// 	console.log(event)
-			// }))
 			!this.modalOpen ? this.modalOpen = true : this.modalOpen = false
 		},
 		formButton () {
-			console.log('formButton')
+			let storage = window.localStorage
+			let app = this
+			if (this.step == 1) {
+				if (app.errors.items.length == 0) {
+					this.goStep(2)
+					storage.setItem('form_name', app.form_name)
+					storage.setItem('form_surname', app.form_surname)
+					storage.setItem('form_organization', app.form_organization)
+					storage.setItem('form_email', app.form_email)
+				}
+			} else if (this.step == 2) {
+				this.send()
+			}
+		},
+		checkStorage () {
+			let app = this
+			let storage = window.localStorage
+			let keys = ['form_name', 'form_surname', 'form_organization', 'form_email']
+			storage.getItem('form_name') && storage.getItem('form_name') != 'null' ? this.form_name = storage.getItem('form_name') : false
+			storage.getItem('form_surname') && storage.getItem('form_surname') != 'null' ? this.form_surname = storage.getItem('form_surname') : false
+			storage.getItem('form_organization') && storage.getItem('form_organization') != 'null' ? this.form_organization = storage.getItem('form_organization') : false
+			storage.getItem('form_email') && storage.getItem('form_email') != 'null' ? this.form_email = storage.getItem('form_email') : false
 		}
 	}
 }
@@ -163,8 +199,8 @@ div.Pergunta {
 	color: #FFF;
 	position: relative;
 	z-index: 0;
+	transition: all ease-in-out .2s;
 	main {
-		padding: 1rem;
 		& > .icon-dialogo {
 			position: absolute;
 			top: -0.25rem;
@@ -173,9 +209,11 @@ div.Pergunta {
 			padding: 0.5rem;
 			border-radius: 2rem;
 			box-shadow: 0 2px 2px $sombra-2;
+			z-index: 1;
 		}
 		& > p.text {
-			margin: 2rem 0 0;
+			margin: 0;
+			padding: 3rem 1rem 1rem;
 			font-size: 1.2rem;
 		}
 		& > button.modalBtn {
@@ -210,6 +248,7 @@ div.Pergunta {
 			transform: scale(1);
 			transform-origin: top right;
 			transition: transform ease-in .2s;
+			z-index: 2;
 			p {
 				margin: 0;
 			}
@@ -234,13 +273,26 @@ div.Pergunta {
 		background-color: $sombra-4;
 		z-index: 0;
 		display: flex;
-		flex-flow: column nowrap;
+		flex-flow: row nowrap;
 		align-items: flex-start;
-		justify-content: center;
-		height: 6rem;
+		justify-content: flex-start;
+		overflow-x: hidden;
+		overflow-y: auto;
+		transition: all ease-in-out .2s;
+		&::after {
+			content: '';
+			position: absolute;
+			bottom: -4px;
+			right: 0;
+			left: 0;
+			max-height: 4px;
+			height: 100%;
+			border-radius: 0 0 4px 4px;
+			background-color: rgba(255, 255, 255, .24);
+			transition: all ease-in-out .4s;
+		}
 		& > button.formStart {
-			min-height: 75%;
-			max-height: 75%;
+			height: 3rem;
 			border: none;
 			background-color: $vermelho;
 			color: #FFF;
@@ -249,48 +301,60 @@ div.Pergunta {
 			padding: 0;
 			border-radius: 0.25rem;
 			box-shadow: 0 2px 4px $sombra-1;
-			padding: 0 1.5rem 0 0;
+			padding: 0 1rem 0 0;
 			overflow: hidden;
+			transition: all ease-in-out .2s;
+			transform: translateX(0);
+			flex: 0.37;
+			text-align: left;
 			.icon-responder {
 				display: inline-flex;
 				align-items: center;
 				height: 100%;
 				background-color: $sombra-4;
-				padding: 0 1.25rem;
-				margin-right: 1.5rem;
+				padding: 0 0.75rem;
+				margin-right: 0.75rem;
 				vertical-align: middle;
 			}
 			&, & > * { cursor: pointer; }
+			&:hover {
+				background-color: $vermelho-escuro;
+			}
 			&.hidden {
 				transform: translateX(-2rem);
-				min-height: 0;
-				max-height: 0;
 				opacity: 0;
 				user-select: none;
 				z-index: -1;
+				flex: 0;
+				padding: 0;
+				height: 0;
 			}
 		}
 		& > form {
 			height: 100%;
+			width: 100%;
+			flex: 1;
+			opacity: 1;
+			transition: all ease-in-out .2s;
 			fieldset {
 				height: 100%;
+				width: 100%;
+				min-width: 0;
 				padding: 0;
 				margin: 0;
 				border: none;
 				transform-origin: left center;
-				display: flex;
-				flex-flow: row nowrap;
-				align-items: flex-start;
 				& > div {
-					width: calc(50% - 0.5rem);
+					display: inline-block;
+					width: calc(25% - 0.75rem);
 					&:not(:first-child) { margin-left: 1rem; }
-					margin-bottom: 1rem;
 					label {
 						display: block;
 						font-size: small;
 						line-height: 1;
 						padding-bottom: 0.25rem;
 						color: $cinza-2;
+						white-space: pre;
 					}
 					input {
 						width: 100%;
@@ -298,26 +362,116 @@ div.Pergunta {
 						border: none;
 						background-color: rgba(255, 255, 255, .08);
 						border-bottom: 2px solid rgba(255, 255, 255, .16);
-						padding: 0.25rem 0.5rem;
+						padding: 0.25rem 0.45rem;
 						font-family: $grotesca;
 						color: #FFF;
-						&:focus {
-							border-color: #FFF;
+						transition: all ease-in .1s;
+						&:focus { border-color: #FFF; }
+						&.inputErro {
+							border-color: $vermelho;
 						}
 					}
+				}
+				& > textarea {
+					width: 100%;
+					height: 100%;
+					min-height: 4rem;
+					margin: 0;
+					padding: 0;
+					background-color: transparent;
+					border: none;
+					font-family: $grotesca;
+					color: #FFF;
 				}
 				&.hidden {
 					z-index: -1;
 					display: none;
 					max-height: 0;
+					flex: 0;
 				}
 			}
 			&.hidden {
-				max-height: 0;
+				opacity: 0;
+				max-width: 0;
 			}
 		}
 		& > button.formBtn {
-			display: none;
+			position: absolute;
+			bottom: 0.75rem;
+			right: 0.75rem;
+			border: none;
+			padding: 0.5rem;
+			border-radius: 2rem;
+			background-color: $vermelho;
+			font-family: $grotesca;
+			font-size: 1rem;
+			color: #FFF;
+			box-shadow: 0 2px 4px $sombra-1;
+			display: flex;
+			align-items: center;
+			opacity: 1;
+			transition: all ease-in-out .2s;
+			&, & > * { cursor: pointer; }
+			& > .icon { user-select: none; }
+			& > span {
+				display: inline-block;
+				letter-spacing: -0.47rem;
+				max-width: 0px;
+				overflow: hidden;
+				transition: all ease-in-out .2s;
+			}
+			&.enviar {
+				& > span {
+					padding-left: 0.25rem;
+					letter-spacing: 0;
+					max-width: 10rem;
+				}
+			}
+			&.hidden { opacity: 0; }
+			&:active { background-color: $vermelho-escuro; }
+			&:disabled {
+				background-color: $cinza-2;
+				color: $cinza-1;
+			}
+		}
+		& > div {
+			display: flex;
+			max-width: 100%;
+			width: 100%;
+			justify-content: space-between;
+			align-items: center;
+			opacity: 1;
+			p {
+				margin: 0;
+			}
+			button {
+				background-color: transparent;
+				padding: 0;
+				border: none;
+				font-family: $grotesca;
+				color: #FFF;
+				font-size: 1rem;
+				&, & > * {
+					cursor: pointer;
+				}
+			}
+			&.enviando {}
+			&.hidden {
+				opacity: 0;
+				max-width: 0;
+			}
+		}
+		&.tosend {
+			background-color: rgba(255, 255, 255, .04);
+			&::after {
+				bottom: 0;
+			}
+		}
+		&.enviando {
+			&::after {
+				bottom: 0;
+				max-height: 100%;
+			}
 		}
 	}
 	&.fetching {
@@ -342,6 +496,52 @@ div.Pergunta {
 	}
 	&.erro {
 		background-color: $vermelho;
+	}
+	@media (max-width: 700px) {
+		margin: 2rem 1rem;
+		max-width: 100%;
+		main {
+			& > p.text {
+				margin: 1.5rem 0 0;
+				font-size: 1rem;
+			}
+		}
+		aside {
+			button.formStart {
+				min-width: 100%;
+				&.hidden { min-width: 0; }
+			}
+			form {
+				min-width: 100%;
+				&.hidden {
+					min-width: 0;
+				}
+				fieldset {
+					width: 100%;
+					flex-flow: column nowrap;
+					& > div {
+						width: 100%;
+						&:not(:first-child) {
+							margin-left: 0;
+							margin-top: 0.5rem;
+						}
+					}
+					& > textarea {
+						height: 6rem;
+					}
+				}
+			}
+			& > div {
+				flex-flow: column nowrap;
+				align-items: flex-start;
+				p {
+					font-size: small;
+				}
+				button {
+					margin-top: 0.5rem;
+				}
+			}
+		}
 	}
 }
 

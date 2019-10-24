a509bd49f0602b3b6a13f9dc0285c6c28c5d23fb
davimh <hosogiri.davi@gmail.com>
2019-10-24 12:12:21 -0300

    Separando componente da view

--- src/components/PanImageMap.vue
+++ src/components/PanImageMap.vue
@@ -1,14 +1,12 @@
 <template>
 	<!-- v-observe-visibility="{ callback: (isVisible, entry) => visibilityChanged(isVisible, entry, dados.url), once: true }" -->
 	<div class="PanImageMap">
-		<main :style="setGrid(0)">
-			<template v-if="zoom == 0">
-				<div class="slice" style="setGrid(1, 'zoomout.jpg')"></div>
-			</template>
-			<template v-if="zoom == 1">
-				<div class="slice" v-for="slice in imgArr" :style="setGrid(1, slice.src)" v-observe-visibility="{ callback: (isVisible, entry) => visibilityChanged(isVisible, entry, slice.src), once: true }">
-				</div>
-			</template>
+		<nav>
+			<input type="range" min="0" max="1" value="0" @change="changeZoom($event)">
+		</nav>
+		<main :style="containerStyle()" ref="container" @drag="method($event)">
+			<div class="slice" v-for="slice in zoom[activezoom]" :style="cellStyle(slice.src)" v-observe-visibility="{ callback: (isVisible, entry) => visibilityChanged(isVisible, entry, slice.src), once: true }">
+			</div>
 		</main>
 	</div>
 </template>
@@ -22,8 +20,8 @@ export default {
 	data () {
 		return {
 			isVisible: false,
-			imgArr: [],
-			zoom: 1
+			zoom: [],
+			activezoom: 0
 		}
 	},
 	props: {
@@ -37,7 +35,12 @@ export default {
 	},
 	computed: {
 		totalSlices () { return this.$props.data.cols * this.$props.data.rows },
-		sliceRatio () { return (this.$props.data.sliceH / this.$props.data.sliceW) * 100 }
+		sliceRatio () { return (this.$props.data.sliceH / this.$props.data.sliceW) * 100 },
+		imageRatio () {
+			let w = this.$props.data.cols * this.$props.data.sliceW
+			let h = this.$props.data.rows * this.$props.data.sliceH
+			return h / w * 100
+		}
 	},
 	created () {
 		this.getImages()
@@ -51,27 +54,33 @@ export default {
 		},
 		getImages () {
 			let base = this.$store.getters.basePath + 'arquivos/' + this.$route.path.slice(1,this.$route.path.length) + '/' + this.$props.data.dir
+			// definindo a array zoom[0]
+			let imageAlone = [ { src: base + '/zoomout.jpg' } ]
+			this.zoom.push(imageAlone)
+			// definindo a array zoom[1]
+			let slices = []
 			for (var i = 1; i <= this.totalSlices; i++) {
 				let thisPath = base + '/__' + i.toString().padStart(2, '0') + '.jpg'
-				this.imgArr.push({ src: thisPath })
+				slices.push({ src: thisPath })
+			}
+			this.zoom.push(slices)
+		},
+		containerStyle () {
+			if (this.activezoom == 0) return `gridTemplateColumns: 1fr; gridTemplateRows: 1fr; }`
+			else if (this.activezoom == 1) return `gridTemplateColumns: repeat(${this.$props.data.cols}, 1fr); gridTemplateRows: repeat(${this.$props.data.rows}, 1fr)`
+		},
+		cellStyle (path) {
+			if (this.activezoom == 0) {
+				return `backgroundImage: url('${path}'); paddingTop: ${this.imageRatio}%;`
+			} else if (this.activezoom == 1) {
+				return `backgroundImage: url('${path}'); paddingTop: ${this.sliceRatio}%; width: ${this.data.sliceW}px`;
 			}
 		},
-		setGrid (level, path) {
-			let declare = ''
-			if (level == 0) {
-				this.zoom == 1 ? () => {
-					declare = `gridTemplateColumns: repeat(${this.$props.data.cols}, 1fr); gridTemplateRows: repeat(${this.$props.data.rows}, 1fr)`
-				} : () => {
-					declare = `gridTemplateColumns: 1fr; gridTemplateRows: 1fr)`
-				}
-			} else if (level == 1) {
-				this.zoom == 1 ? () => {
-					declare = `backgroundImage: url('${path}'); paddingTop: ${this.sliceRatio}%; width: ${this.data.sliceW}px`
-				} : () => {
-					declare = `backgroundImage: url('${this.$store.getters.basePath}arquivos/${this.$route.path.slice(1,this.$route.path.length)}/${this.$props.data.dir}/${path}')`
-				}
-			}
-			return declare
+		changeZoom (e) {
+			this.activezoom = e.target.value
+		},
+		method (e) {
+			console.log(e)
 		}
 	}
 }
@@ -86,15 +95,24 @@ export default {
 }
 
 .PanImageMap {
-	max-width: 80vw;
-	max-height: 80vh;
-	overflow: auto;
+	width: 80vw;
+	height: 80vh;
 	margin: 0 auto;
+	position: relative;
+	nav {
+		position: absolute;
+		bottom: -3rem;
+		left: 50%;
+		transform: translateX(-50%);
+	}
 	main {
 		display: grid;
 		grid-gap: 0;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
 		.slice {
-			background-size: contain;
+			background-size: cover;
 		}
 	}
 }

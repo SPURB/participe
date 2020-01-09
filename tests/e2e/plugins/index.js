1b169670de89642fca22aecfb4050ec0aea9b6bd
Thomas Yuba <yubathom@gmail.com>
2020-01-09 02:01:53 -0300

    test: fix travis

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const path = '.env.development.local'
 require('dotenv').config({ path: path})
 
-if (!fs.existsSync(path)) {
+if (!fs.existsSync(path) && !process.env.CI) {
 	throw new Error ('Testes e2e necessitam de arquivo ".env.development.local" na raiz do projeto. Solicite com SMDU ou SPUrbanismo.')
 }
 
@@ -10,5 +10,8 @@ module.exports = (on, config) => {
 	config.env = process.env
 	config.env.apiVersion = 'v2'
 
+	if (process.env.CI) {
+		config.env.VUE_APP_API_URL='https://api.gestaourbana.prefeitura.sp.gov.br/'
+	}
 	return config
 }

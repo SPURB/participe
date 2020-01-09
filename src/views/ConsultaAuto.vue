903f28a07ca5a0872d9cb71428185e181e43e93b
Renan <renan.smul@gmail.com>
2020-01-09 12:06:10 -0300

    Criado componente curinga para exibir consultas geradas automaticamente (ConsultaAuto); Alterada versao da API (v2 > v3);
    Signed-off-by: Renan <renan.smul@gmail.com>

---
+++ src/views/ConsultaAuto.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="ConsultaAuto" ref="conteudoConsulta">
+		<PageTop background_image_src="arquivos/capas/placeholder_480w.jpg" :esta_consulta="estaConsulta">
+			<template slot="titulo"><div>Título da Nova Consulta</div></template>
+			<template slot="subtitulo"><div>Subtítulo da Nova Consulta</div></template>
+		</PageTop>
+		<Indice :titulos="titulosLimpo"></Indice>
+
+		<section>
+			<h2 class="titulo" indent="1">Apresentação</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magnam soluta nisi illum temporibus maiores? Fuga obcaecati cumque eos? Repudiandae minus accusamus mollitia blanditiis saepe non, eveniet sit sapiente eum.</p>
+			<Comments :attr="{id:commentId(), context:'Apresentação'}" v-if="estaConsulta.ativo == 1"></Comments>
+		</section>
+
+		<section>
+			<h2 class="titulo" indent="1">Seção 1</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident deleniti quae iure, quia recusandae eos! Recusandae culpa quod dolorum excepturi amet voluptatum perferendis doloribus, minus commodi. Dignissimos odit ipsam nam?</p>
+			<Comments :attr="{id:commentId(), context:'Seção 1'}" v-if="estaConsulta.ativo == 1"></Comments>
+		</section>
+
+		<section>
+			<h2 class="titulo" indent="1">Seção 2</h2>
+			<p>Seção 2</p>
+		</section>
+
+		<section ref="allComments">
+			<h2 v-show="commentsLoaded" class="titulo" indent="1">Contribuições</h2>
+			<CommentsLoader :attr="estaConsulta"></CommentsLoader>
+			<Comments v-if="commentsLoaded && parseInt(estaConsulta.ativo) === 1" :attr="{id:commentId(), context:'Comentarios'}"></Comments>
+
+		</section>
+	</div>
+</template>
+
+<script>
+import PageTop from '@/components/PageTop'
+import Indice from '@/components/Indice'
+import { consultasCommons } from '@/mixins/consultasCommons'
+import Comments from '@/components/Comments'
+import CommentsLoader from '@/components/CommentsLoader'
+
+
+export default {
+	name: 'ConsultaAuto',
+	data () {
+		return {
+			titulosLimpo: [],
+			comments_atrr: undefined,
+			consultas: false,
+			estaConsulta: {}
+		}
+	},
+	components: {
+		Comments,
+		CommentsLoader,
+		PageTop,
+		Indice
+	},
+	mixins: [ consultasCommons ]
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../variables';
+@import '../consulta';
+@import '../oldstylestoprint';
+
+</style>

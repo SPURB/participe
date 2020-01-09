8c0e3e1f5a251bf552f64a53544c85cf17a874b6
Thomas Yuba <yubathom@gmail.com>
2020-01-09 01:02:22 -0300

    test: resolve #163
    * test: integra testes com travis-ci
    
    * dep: reverte @vue/eslint-config-standard para v4
    
    * doc: inclui status de testes em readme

--- tests/e2e/integration/home.spec.js
+++ tests/e2e/integration/home.spec.js
@@ -1,22 +1,7 @@
-// https://docs.cypress.io/api/introduction/api.html
-
 describe('Home', () => {
 	it('Existe h1 com substring "participe"', () => {
-		cy.seedAndVisit()
+		cy.visit('/')
 			.get('h1')
 			.should('contain','participe')
-	})
-
-	it('Número de consultas na DOM é o mesmo dos elementos array da resposta da API (tabela "consultas")', () => {
-		let nConsultas
-		let nConsultasDOM = 0
-
-		cy.seedAndVisit()
-			.then(res => nConsultas = res.response.body.length)
-			.get('section.abertas').children('ul').children('li')
-			.then(itens => nConsultasDOM = nConsultasDOM + itens.length)
-			.get('section.encerradas').children('ul').children('li')
-			.then(itens => nConsultasDOM = nConsultasDOM + itens.length )
-			.should(() => expect(nConsultas === nConsultasDOM).to.equal(true, 'Há mesmo número de itens na API e nas listas da DOM (soma de abertas e encerradas)'))
 	})
 })

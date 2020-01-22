66d15f1061c9066c15c46f0b9d23750a37c43a16
yubathom <yubathom@gmail.com>
2020-01-22 12:46:02 -0300

    chore: atualiza fechadura

--- src/components/graf/arco-tiete-2/PizzaCustos.vue
+++ src/components/graf/arco-tiete-2/PizzaCustos.vue
@@ -1,49 +1,95 @@
 <template>
 	<div class="PizzaCustos">
-		<div id="g-7_Grafico_custo_ACT_320px-box" class="ai2html ai2html-box-v5">
-			<!-- Artboard: custo -->
-			<div id="g-7_Grafico_custo_ACT_320px-custo" class="g-artboard" data-aspect-ratio="0.561" data-min-width="320">
-				<img id="g-7_Grafico_custo_ACT_320px-custo-img" class="g-aiImg" :src="bgSrc"/>
-				<div id="g-ai0-1" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:3.6323%;margin-top:-9.7px;left:2.5613%;width:325px;">
+		<div id="g-7_Grafico_custo_ACT_320px-custo-box" class="ai2html ai2html-box-v5">
+			<!-- Artboard: Prancheta_1 -->
+			<div id="g-7_Grafico_custo_ACT_320px-custo-Prancheta_1" class="g-artboard" data-aspect-ratio="0.403" data-min-width="320">
+				<img id="g-7_Grafico_custo_ACT_320px-custo-Prancheta_1-img" class="g-aiImg" :src="bgSrc"/>
+				<div id="g-ai0-1" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:3.6176%;margin-top:-9.7px;left:3.1235%;width:325px;">
 					<p class="g-pstyle0">% DA ESTIMATIVA TOTAL DE CUSTOS BÁSICOS</p>
 				</div>
-				<div id="g-ai0-2" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:11.1858%;margin-top:-7.8px;left:47.5388%;width:38px;">
+				<div id="g-ai0-2" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:8.0206%;margin-top:-9.6px;left:45.9845%;width:41px;">
 					<p class="g-pstyle1">2%</p>
 				</div>
-				<div id="g-ai0-3" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:16.449%;margin-top:-7.8px;left:23.8083%;width:38px;">
-					<p class="g-pstyle1">2%</p>
-				</div>
-				<div id="g-ai0-4" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:17.3262%;margin-top:-7.8px;left:37.9758%;width:45px;">
+				<div id="g-ai0-3" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:9.2809%;margin-top:-9.6px;left:30.2684%;width:50px;">
 					<p class="g-pstyle1">10%</p>
 				</div>
-				<div id="g-ai0-5" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:26.2736%;margin-top:-7.8px;left:23.9255%;width:45px;">
+				<div id="g-ai0-4" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:10.1632%;margin-top:-9.6px;left:69.9115%;width:50px;">
+					<p class="g-pstyle1">17%</p>
+				</div>
+				<div id="g-ai0-5" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:12.8098%;margin-top:-9.6px;left:20.0536%;width:41px;">
+					<p class="g-pstyle1">2%</p>
+				</div>
+				<div id="g-ai0-6" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:18.9853%;margin-top:-9.6px;left:9.1408%;width:50px;">
 					<p class="g-pstyle1">12%</p>
 				</div>
-				<div id="g-ai0-6" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:32.414%;margin-top:-7.8px;left:73.2614%;width:45px;">
-					<p class="g-pstyle1">50%</p>
+				<div id="g-ai0-7" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:32.7226%;margin-top:-9.6px;left:82.9706%;width:50px;">
+					<p class="g-pstyle1">33%</p>
 				</div>
-				<div id="g-ai0-7" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:40.6596%;margin-top:-7.8px;left:29.8581%;width:45px;">
+				<div id="g-ai0-8" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:35.1172%;margin-top:-9.6px;left:18.9433%;width:50px;">
 					<p class="g-pstyle1">25%</p>
 				</div>
-				<div id="g-ai0-8" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:55.0457%;margin-top:-7.8px;left:38.4561%;width:144px;">
-					<p class="g-pstyle1">REDE DE MOBILIDADE</p>
+				<div id="g-ai0-9" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:42.82%;margin-top:-7.8px;left:6.4404%;width:183px;">
+					<p class="g-pstyle2">REDE DE MOBILIDADE ATIVA</p>
 				</div>
-				<div id="g-ai0-9" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:62.1256%;margin-top:-8.1px;left:38.4561%;width:89px;">
-					<p class="g-pstyle1">HABITAÇÃO</p>
+				<div id="g-ai0-10" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:44.8365%;margin-top:-7.8px;left:6.4404%;width:301px;">
+					<p class="g-pstyle2">Requalificação de viários existentes , melhoria dos </p>
 				</div>
-				<div id="g-ai0-10" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:70.5195%;margin-top:-15px;left:38.4561%;width:112px;">
-					<p class="g-pstyle1">EQUIPAMENTOS</p>
-					<p class="g-pstyle1">PÚBLICOS </p>
+				<div id="g-ai0-11" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:46.853%;margin-top:-7.8px;left:6.4404%;width:306px;">
+					<p class="g-pstyle2">passeios existentes, transposições não motorizadas</p>
 				</div>
-				<div id="g-ai0-11" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:76.3098%;margin-top:-8px;left:38.4561%;width:101px;">
-					<p class="g-pstyle1">REDE HÍDRICA</p>
+				<div id="g-ai0-12" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:50.6339%;margin-top:-7.8px;left:6.4404%;width:145px;">
+					<p class="g-pstyle2">REDE DE MOBILIDADE</p>
 				</div>
-				<div id="g-ai0-12" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:84.7301%;margin-top:-15px;left:38.4561%;width:130px;">
-					<p class="g-pstyle1">REDE DE ESPAÇOS </p>
-					<p class="g-pstyle1">PÚBLICOS</p>
+				<div id="g-ai0-13" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:52.6504%;margin-top:-7.8px;left:6.4404%;width:295px;">
+					<p class="g-pstyle2">Abertura de novos viários, alargamento de viários </p>
 				</div>
-				<div id="g-ai0-13" class="g-Gr_fico g-aiAbs g-aiPointText" style="top:90.4845%;margin-top:-7.8px;left:38.4561%;width:123px;">
-					<p class="g-pstyle1">INFRAESTRUTURA</p>
+				<div id="g-ai0-14" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:54.6669%;margin-top:-7.8px;left:6.4404%;width:139px;">
+					<p class="g-pstyle2">existentes, pontilhões</p>
+				</div>
+				<div id="g-ai0-15" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:58.7446%;margin-top:-8.1px;left:6.4404%;width:89px;">
+					<p class="g-pstyle2">HABITAÇÃO</p>
+				</div>
+				<div id="g-ai0-16" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:60.8423%;margin-top:-7.8px;left:6.4404%;width:255px;">
+					<p class="g-pstyle2">Urbanização de assentamentos precários, </p>
+				</div>
+				<div id="g-ai0-17" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:62.8588%;margin-top:-7.8px;left:6.4404%;width:323px;">
+					<p class="g-pstyle2">requalificação de conjuntos habitacionais, provisão de </p>
+				</div>
+				<div id="g-ai0-18" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:65.0014%;margin-top:-7.8px;left:6.4404%;width:67px;">
+					<p class="g-pstyle2">moradia</p>
+				</div>
+				<div id="g-ai0-19" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:69.0602%;margin-top:-8px;left:6.4404%;width:175px;">
+					<p class="g-pstyle2">EQUIPAMENTOS PÚBLICOS</p>
+				</div>
+				<div id="g-ai0-20" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:71.0508%;margin-top:-7.8px;left:6.4404%;width:312px;">
+					<p class="g-pstyle2">Novos equipamentos, equipamentos em edifícios de </p>
+				</div>
+				<div id="g-ai0-21" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:73.0673%;margin-top:-7.8px;left:6.4404%;width:99px;">
+					<p class="g-pstyle2">valor histórico</p>
+				</div>
+				<div id="g-ai0-22" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:77.6303%;margin-top:-8px;left:6.4404%;width:101px;">
+					<p class="g-pstyle2">REDE HÍDRICA</p>
+				</div>
+				<div id="g-ai0-23" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:79.6209%;margin-top:-7.8px;left:6.4404%;width:295px;">
+					<p class="g-pstyle2">Canteiros drenantes, áreas de absorção, áreas de </p>
+				</div>
+				<div id="g-ai0-24" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:81.6374%;margin-top:-7.8px;left:6.4404%;width:89px;">
+					<p class="g-pstyle2">acumulação</p>
+				</div>
+				<div id="g-ai0-25" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:86.2003%;margin-top:-8px;left:6.4404%;width:188px;">
+					<p class="g-pstyle2">REDE DE ESPAÇOS PÚBLICOS</p>
+				</div>
+				<div id="g-ai0-26" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:88.317%;margin-top:-7.8px;left:6.4404%;width:290px;">
+					<p class="g-pstyle2">Requalificação de áreas verdes existentes, áreas </p>
+				</div>
+				<div id="g-ai0-27" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:90.3335%;margin-top:-7.8px;left:6.4404%;width:194px;">
+					<p class="g-pstyle2">verdes novas, caminhos verdes</p>
+				</div>
+				<div id="g-ai0-28" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:95.1226%;margin-top:-7.8px;left:6.4404%;width:124px;">
+					<p class="g-pstyle2">INFRAESTRUTURA</p>
+				</div>
+				<div id="g-ai0-29" class="g-Grafico_2 g-aiAbs g-aiPointText" style="top:97.1391%;margin-top:-7.8px;left:6.4404%;width:243px;">
+					<p class="g-pstyle2">Remanejamento de linhas de alta tensão</p>
 				</div>
 			</div>
 		</div>
@@ -70,10 +116,10 @@ export default {
 	}
 }
 
-#g-7_Grafico_custo_ACT_320px-box .g-artboard {
+#g-7_Grafico_custo_ACT_320px-custo-box .g-artboard {
 	margin:0 auto;
 }
-#g-7_Grafico_custo_ACT_320px-box .g-artboard p {
+#g-7_Grafico_custo_ACT_320px-custo-box .g-artboard p {
 	margin:0;
 }
 .g-aiAbs {
@@ -88,12 +134,12 @@ export default {
 	box-sizing: border-box;
 }
 .g-aiPointText p { white-space: nowrap; }
-#g-7_Grafico_custo_ACT_320px-custo {
+#g-7_Grafico_custo_ACT_320px-custo-Prancheta_1 {
 	position:relative;
 	overflow:hidden;
 	width:320px;
 }
-#g-7_Grafico_custo_ACT_320px-custo p {
+#g-7_Grafico_custo_ACT_320px-custo-Prancheta_1 p {
 	font-family:'roboto',segoe ui,myriad pro,arial,helvetica,sans-serif;
 	font-weight:400;
 	font-size:12px;
@@ -111,13 +157,18 @@ export default {
 	mix-blend-mode:normal;
 	font-style:normal;
 }
-#g-7_Grafico_custo_ACT_320px-custo .g-pstyle0 {
+#g-7_Grafico_custo_ACT_320px-custo-Prancheta_1 .g-pstyle0 {
 	font-weight:700;
 	font-size:14px;
 	line-height:17px;
 	height:17px;
 }
-#g-7_Grafico_custo_ACT_320px-custo .g-pstyle1 {
+#g-7_Grafico_custo_ACT_320px-custo-Prancheta_1 .g-pstyle1 {
+	font-size:14px;
+	line-height:17px;
+	height:17px;
+}
+#g-7_Grafico_custo_ACT_320px-custo-Prancheta_1 .g-pstyle2 {
 	height:14px;
 }
 </style>

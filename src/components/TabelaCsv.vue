9fa05e60808e7cc0227a2bbe2d425d5c0564922c
yubathom <yubathom@gmail.com>
2020-03-30 10:25:57 -0300

    style: padroniza identação em TabelaCsv

--- src/components/TabelaCsv.vue
+++ src/components/TabelaCsv.vue
@@ -1,172 +1,171 @@
 <template>
-  <div class="table-csv">
-    <table>
-      <caption>{{ titulo }}</caption>
-      <thead>
-        <tr>
-          <th
-            scope="col"
-            :key="index"
-            v-for="(col, index) in colunas">
-            {{ col }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          :key="`tr-`+index"
-          :class="index%2 === 0 ? 'linha-default' : 'linha-escura'"
-          v-for="(item, index) in totalLinhas">
-            <td
-              :data-label="colunas[indexLinha]"
-              :key="`td-`+indexLinha"
-              :colspan="countColspan(linhasValues[index].length, totalColunas)"
-              v-for="(linha, indexLinha) in linhasValues[index]"
-            >
-              {{ linha }}
-            </td>
-        </tr>
-      </tbody>
-    </table>
-    <div class="fonte" v-if="fonte !== ''">
-      fonte: <span>{{ fonte }}</span>
-    </div>
-  </div>
+	<div class="table-csv">
+		<table>
+			<caption>{{ titulo }}</caption>
+			<thead>
+				<tr>
+					<th
+						scope="col"
+						:key="index"
+						v-for="(col, index) in colunas">
+						{{ col }}
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr
+					:key="`tr-`+index"
+					:class="index%2 === 0 ? 'linha-default' : 'linha-escura'"
+					v-for="(item, index) in totalLinhas">
+						<td
+							:data-label="colunas[indexLinha]"
+							:key="`td-`+indexLinha"
+							:colspan="countColspan(linhasValues[index].length, totalColunas)"
+							v-for="(linha, indexLinha) in linhasValues[index]"
+						>
+							{{ linha }}
+						</td>
+				</tr>
+			</tbody>
+		</table>
+		<div class="fonte" v-if="fonte !== ''">
+			fonte: <span>{{ fonte }}</span>
+		</div>
+	</div>
 </template>
 
 <script>
-/* eslint-disable indent */
 export default {
-  name: 'TabelaCsv',
-  props: {
-    titulo: {
-      type: String,
-      required: true
-    },
-    colunas: {
-      type: Array,
-      required: true
-    },
-    linhas: {
-      type: Array,
-      required: true
-    },
-    fonte: {
-      type: String,
-      default: ''
-    }
-  },
-  computed: {
-    linhasValues () {
-      let linhas = this.linhas
-      linhas.forEach((linha, index) => {
-        linhas[index] = [...new Set(linha)]
-      })
-      return linhas
-    },
-    totalColunas () {
-      return this.colunas.length
-    },
-    totalLinhas () {
-      return this.linhas.length
-    }
-  },
-  methods: {
-    countColspan (linha, total) {
-      if (linha === 1) return total
-      if (linha < total) return linha
-    }
-  }
+	name: 'TabelaCsv',
+	props: {
+		titulo: {
+			type: String,
+			required: true
+		},
+		colunas: {
+			type: Array,
+			required: true
+		},
+		linhas: {
+			type: Array,
+			required: true
+		},
+		fonte: {
+			type: String,
+			default: ''
+		}
+	},
+	computed: {
+		linhasValues () {
+			let linhas = this.linhas
+			linhas.forEach((linha, index) => {
+				linhas[index] = [...new Set(linha)]
+			})
+			return linhas
+		},
+		totalColunas () {
+			return this.colunas.length
+		},
+		totalLinhas () {
+			return this.linhas.length
+		}
+	},
+	methods: {
+		countColspan (linha, total) {
+			if (linha === 1) return total
+			if (linha < total) return linha
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../variables';
 .table-csv {
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
 
-  .fonte {
-    font-size: 13pt;
-    color: $preto;
-    opacity: 0.8;
-    width: 90%;
-  }
+	.fonte {
+		font-size: 13pt;
+		color: $preto;
+		opacity: 0.8;
+		width: 90%;
+	}
 
-  table {
-    border-collapse: collapse;
-    background-color: #FFF;
-    margin: 0;
-    width: 90%;
+	table {
+		border-collapse: collapse;
+		background-color: #FFF;
+		margin: 0;
+		width: 90%;
 
-    caption {
-      font-size: 1.5rem;
-      margin-bottom: 3px;
-    }
+		caption {
+			font-size: 1.5rem;
+			margin-bottom: 3px;
+		}
 
-    th, td {
-      border: 1px solid $cinza-2;
-      color: $preto;
-      font-size: smaller;
-      padding: 4px 8px;
-      position: relative;
-    }
+		th, td {
+			border: 1px solid $cinza-2;
+			color: $preto;
+			font-size: smaller;
+			padding: 4px 8px;
+			position: relative;
+		}
 
-    thead tr {
-      background-color: $sombra-4;
-    }
-  }
+		thead tr {
+			background-color: $sombra-4;
+		}
+	}
 
-  .linha-escura {
-    background-color: $cinza-3;
-  }
+	.linha-escura {
+		background-color: $cinza-3;
+	}
 }
 @media screen and (max-width: 600px) {
-  table {
-    box-sizing: border-box;
+	table {
+		box-sizing: border-box;
 
-    caption {
-      font-size: 1.3em;
-    }
+		caption {
+			font-size: 1.3em;
+		}
 
-    thead {
-      border: none;
-      clip: rect(0 0 0 0);
-      height: 1px;
-      margin: -1px;
-      overflow: hidden;
-      padding: 0;
-      position: absolute;
-      width: 1px;
-    }
+		thead {
+			border: none;
+			clip: rect(0 0 0 0);
+			height: 1px;
+			margin: -1px;
+			overflow: hidden;
+			padding: 0;
+			position: absolute;
+			width: 1px;
+		}
 
-    tr {
-      display: block;
-      margin-bottom: .625em;
-      box-shadow: 0px 2px 2px 2px $sombra-4;
-      cursor: pointer;
-    }
+		tr {
+			display: block;
+			margin-bottom: .625em;
+			box-shadow: 0px 2px 2px 2px $sombra-4;
+			cursor: pointer;
+		}
 
-    td {
-      border-top: none !important;
-      border-right: none !important;
-      border-left: none !important;
-      border-bottom: 1px solid #ddd !important;
-      display: block;
-      font-size: .8em;
-      text-align: right;
+		td {
+			border-top: none !important;
+			border-right: none !important;
+			border-left: none !important;
+			border-bottom: 1px solid #ddd !important;
+			display: block;
+			font-size: .8em;
+			text-align: right;
 
-      &::before {
+			&::before {
 				content: attr(data-label);
 				font-weight: bold;
-        float: left;
+				float: left;
 				hyphens: auto;
 				padding-right: 4px;
 				text-align: left;
 			}
-    }
-  }
+		}
+	}
 }
 </style>

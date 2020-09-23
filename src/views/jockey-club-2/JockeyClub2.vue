10a20243e13cb2e9c88fe4e1b7cc551ac616e841
m-dantas <mauricio.dantascp@gmail.com>
2020-09-23 11:13:27 -0300

    fix: corrige ordem dos arquivos

--- src/views/jockey-club-2/JockeyClub2.vue
+++ src/views/jockey-club-2/JockeyClub2.vue
@@ -6,18 +6,6 @@
 					<input type="radio" name="tabs" id="item2">
 					<label for="item2">Arquivos</label>
 					<div class="conteudoTab">
-						<h4 class="jockey-club2__section-title">Anexos do Decreto:</h4>
-						<h5 class="jockey-club2__section-item">Mapas</h5>
-						<ul class="links">
-							<li>
-								<a :href="`${base}Anexo1_Mapa1.pdf`" download>
-									<i class="icon-pdf icon"><span>pdf</span></i>
-									<div>Mapa 1<br>Perímetro de regulação urbanística e setores</div>
-									<span>1.2 Mb</span>
-								</a>
-							</li>
-						</ul>
-
 						<h4 class="jockey-club2__section-title">Cadernos do PIU:</h4>
 						<h5 class="jockey-club2__section-item">Cadernos</h5>
 						<ul class="links">
@@ -46,6 +34,18 @@
 									<i class="icon-pdf icon"><span>pdf</span></i>
 									<div>Modelo de gestão democrática</div>
 									<span>26.3 mb kb</span>
+								</a>
+							</li>
+						</ul>
+
+						<h4 class="jockey-club2__section-title">Anexos do Decreto:</h4>
+						<h5 class="jockey-club2__section-item">Mapas</h5>
+						<ul class="links">
+							<li>
+								<a :href="`${base}Anexo1_Mapa1.pdf`" download>
+									<i class="icon-pdf icon"><span>pdf</span></i>
+									<div>Mapa 1<br>Perímetro de regulação urbanística e setores</div>
+									<span>1.2 Mb</span>
 								</a>
 							</li>
 						</ul>
